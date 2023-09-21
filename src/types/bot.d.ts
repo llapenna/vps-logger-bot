0b3abbc1d24b34b5f63eb8f1a112ec415790ce32
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-09-21 09:06:04 -0300

    feat: add whitelist type

--- src/types/bot.d.ts
+++ src/types/bot.d.ts
@@ -4,3 +4,9 @@ import { BotCommand } from '@telegraf/types';
 export type BotCommandWithHandler = BotCommand & {
   handler: (ctx: Context) => void;
 };
+
+export type Whitelist = {
+  list: number[];
+  add: (id: number) => void;
+  remove: (id: number) => void;
+};
