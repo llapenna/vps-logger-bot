25a93c3a99bb891161caeccd29a83d19bbf3c2f0
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-09-21 14:48:40 -0300

    feat: add prefix to logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,11 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
+const prefix = (type: 'INFO' | 'ERROR' | 'WARN') =>
+  `[${new Date().toLocaleString()} - ${type}]:`;
+
 /**
  * Interface for `console.log`. Won't print on 'production' mode
  * @param args Same arguments as `console.log`
  */
 function info(...args: any[]) {
   if (process.env.NODE_ENV !== 'production') {
-    console.log(...args);
+    console.log(prefix('INFO'), ...args);
   }
 }
 
@@ -14,7 +18,7 @@ function info(...args: any[]) {
  * @param args Same arguments as `console.error`
  */
 function error(...args: any[]) {
-  console.error(...args);
+  console.error(prefix('ERROR'), ...args);
 }
 
 /**
@@ -22,7 +26,7 @@ function error(...args: any[]) {
  * @param args Same arguments as `console.warn`
  */
 function warn(...args: any[]) {
-  console.warn(...args);
+  console.warn(prefix('WARN'), ...args);
 }
 
 const logger = {
