7a619c030a62560b5c1f6438f1ab2ab33cf2efa3
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-09-28 23:09:13 -0300

    chore: add no-console ESLint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
-  rules: {},
+  rules: {
+    'no-console': ['warn', { allow: ['warn'] }],
+  },
 };
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any, no-console */
 
 const prefix = (type: 'INFO' | 'ERROR' | 'WARN') =>
   `[${new Date().toLocaleString()} - ${type}]:`;
