c98d4710dfb37d012dda6bf5520b318731de8871
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-10-01 00:56:21 -0300

    feat: create service file var in install script

--- scripts/install_service.sh
+++ scripts/install_service.sh
@@ -1,14 +1,17 @@
+# Variables
+SERVICE_FILE=$SERVICE_PATH/vps-logger-bot.service
+
 # Create folder for service if not exists
 echo "🔄 Creating service folder" $SERVICE_PATH "..."
 mkdir -p $SERVICE_PATH
 echo "✅ Created!\n"
 
 # Copy service file
-echo "🔄 Copy service file into" $SERVICE_PATH"/vps-logger-bot.service..."
-cp ./vps-logger-bot.service.template $SERVICE_PATH/vps-logger-bot.service
+echo "🔄 Copy service file into" $SERVICE_FILE "..."
+cp ./vps-logger-bot.service.template $SERVICE_FILE
 echo "✅ Copied!\n"
 
 # Replace env variables values
 echo "🔄 Replacing env variables values..."
-sed -i.bak -e s@_LOGFILE_PATH@$LOGFILE_PATH@g -e s@_BOT_TOKEN@$BOT_TOKEN@g $SERVICE_PATH/vps-logger-bot.service
+sed -i.bak -e s@_LOGFILE_PATH@$LOGFILE_PATH@g -e s@_BOT_TOKEN@$BOT_TOKEN@g -e s@_WORKDIR@$HOME@g -e s@_PROJECT_PATH@$PROJECT_PATH@g $SERVICE_PATH/vps-logger-bot.service
 echo "✅ Replaced!\n"
