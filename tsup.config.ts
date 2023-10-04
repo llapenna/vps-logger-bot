554d3721fd568215f3f4b5aff32958583c065466
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-10-04 13:31:25 -0300

    build: add tsup to bundle typescript files
    This enables compatibility with Pure ESM modules

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  splitting: true,
+  sourcemap: false,
+  clean: true,
+  target: 'node16',
+  minify: true,
+  treeshake: true,
+  dts: true,
+  metafile: true,
+  async onSuccess() {
+    console.log('\n');
+  },
+});
