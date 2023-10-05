af759a295ec3deca74a624cc53decd05c981f03f
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-10-04 22:06:32 -0300

    feat: initialize database on start

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import bot from '@/bot';
 import watcher from '@/watcher';
+import database from '@/utils/database';
 
 (async () => {
   // Configure bot
   await bot.registerCommands();
+  // Initialize database by reading the JSON file
+  await database.init();
 
   // Watch for changes in the file
   watcher.start(({ newLines }) => {
