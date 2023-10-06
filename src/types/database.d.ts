c63e3e07206fe2c25453b0113c59c8af1268577c
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-10-06 10:49:18 -0300

    feat: add vps user array to chat db  type

--- src/types/database.d.ts
+++ src/types/database.d.ts
@@ -1,7 +1,11 @@
 export type Entry<T extends object = object> = T[];
 
 // Database Fields
-export type Chats = Entry<{ telegramId: number; broadcast: boolean }>;
+export type Chats = Entry<{
+  telegramId: number;
+  broadcast: boolean;
+  vpsUsers: string[];
+}>;
 
 export interface Database {
   chats: Chats;
