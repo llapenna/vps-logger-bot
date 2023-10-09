91d48409c17f488b9d8252adc95ceed5eae42512
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-10-08 21:51:00 -0300

    feat: add geolocation API response typing

---
+++ src/types/geolocation.d.ts
@@ -0,0 +1,17 @@
+export type GeolocationResponse = {
+  status: 'success' | 'fail';
+  country: string;
+  countryCode: string;
+  region: string;
+  regionName: string;
+  city: string;
+  zip: string;
+  lat: number;
+  lon: number;
+  timezone: string;
+  isp: string;
+  org: string;
+  as: string;
+  query: string;
+  flag: string;
+};
